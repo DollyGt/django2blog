6d6485b79d7a9c22cf85fe003f0cd716ba54ae1c
Richard Dalton <richard@codeinstitute.net>
2018-06-18 13:35:45 +0000

    Allow log in and add author to posts

--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,7 @@ from posts.views import get_posts, post_detail, create_or_edit_post
 
 urlpatterns = [
     path('new/', create_or_edit_post, name='new_post'),
-    path('<pk>/edit', create_or_edit_post, name='edit_post'),
-    path('<pk>/', post_detail, name='post_detail'),
+    path('<int:pk>/edit', create_or_edit_post, name='edit_post'),
+    path('<int:pk>/', post_detail, name='post_detail'),
     path('', get_posts, name='get_posts'),
 ]
